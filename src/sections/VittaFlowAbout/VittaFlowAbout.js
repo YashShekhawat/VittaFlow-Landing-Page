c0b6f0a300030c2582403041d52987a20a45c6ef
Navnish Singh <navnish.singh@synergymachines.us>
2023-07-23 22:33:43 +0530

    Content changes

--- src/sections/VittaFlowAbout/VittaFlowAbout.js
+++ src/sections/VittaFlowAbout/VittaFlowAbout.js
@@ -9,34 +9,36 @@ import { Typography } from "../../components";
 const VittaFlowAbout = () => {
   const data = [
     {
-      heading: "Open Source",
+      heading: "Expert Team of Traders",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et nibh urna in proin dui purus bibendum cras. Morbi cursus nunc.",
+        "Our experts provide personalized investment guidance, considering your financial goals, risk tolerance, and unique circumstances. Maximize returns with our dedicated team's well-informed decisions and tailored recommendations.",
       img: Users,
     },
     {
-      heading: "Worldwide",
+      heading: "Up to 14-18% Monthly Returns",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et nibh urna in proin dui purus bibendum cras. Morbi cursus nunc.",
+        "Maximize your earnings with our skilled team of traders and investors. Enjoy monthly returns of up to 14-18% and withdraw your funds whenever you need them.",
       img: Globe,
     },
     {
-      heading: "Transparent",
+      heading: "Loss Compensation Fund",
       content:
-        "We always help you save and manage your finances, we have a system that can manage expenses that are limited each month",
+        "VittaFlow prioritizes your peace of mind by offering a Loss Compensation Fund. This dedicated fund covers losses caused by our mistakes, ensuring accountability and protection for our clients.",
       img: Search,
     },
     {
-      heading: "Community Driven",
+      heading: "Trust and Commitment",
       content:
-        "We always help you save and manage your finances, we have a system that can manage expenses that are limited each month",
+        "VittaFlow is committed to earning your trust by being a dependable financial partner. Our dedication to your success in the financial market ensures personalized guidance and support for maximizing returns on your investments.",
       img: Compass,
     },
   ];
 
   return (
     <div className={`md:px-28 px-7 md:py-16 py-10`}>
-      <div className={"flex flex-col items-center justify-center gap-14 md:gap-24"}>
+      <div
+        className={"flex flex-col items-center justify-center gap-14 md:gap-24"}
+      >
         <div className={`flex flex-col items-center justify-center gap-2`}>
           <h1 className="text-white">
             About <span className={classes.specialText}>Vitta Flow</span>
@@ -45,14 +47,18 @@ const VittaFlowAbout = () => {
             typeScale={"subText"}
             className={`${classes.subText2} text-center md:w-5/12`}
           >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat
-            nulla suspendisse tortor aenean dis placerat. Scelerisque imperdiet
-            vitae dolor non aliquam. Malesuada.
+            VittaFlow, derived from the Sanskrit word "Vitta", meaning wealth,
+            represents our core objective: to help individuals grow their wealth
+            through intelligent and strategic investments.
           </Typography>
         </div>
         <div className="flex flex-col gap-10 ">
-          <div className={`md:flex justify-between items-center flex-wrap w-full`}>
-            <h2 className={`text-center text-white text-3xl`}>What drives Vitta Flow?</h2>
+          <div
+            className={`md:flex justify-between items-center flex-wrap w-full`}
+          >
+            <h2 className={`text-center text-white text-3xl`}>
+              What drives Vitta Flow?
+            </h2>
             <Typography
               typeScale={"subText"}
               className={`text-white md:w-4/12 text-center md:visible hidden`}
